c2d143cf7ede000e78b671beb631bf99e8335fd0
dreamshrine <shayne.e.s.atkins@gmail.com>
2026-03-31 21:14:37 -0400

    2-player setup complete

--- src/GameSetup.java
+++ src/GameSetup.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 public class GameSetup {
     static Scanner scanner = new Scanner(System.in);
 
-    static void setupTwoPlayer() {}
+    static void setupTwoPlayer() {
+        System.out.print("Player 1, please enter your name: ");
+        String name1 = scanner.nextLine().trim();
+
+        char symbol1 = promptSymbol(name1);
+        char symbol2 = (symbol1 == 'X') ? 'O' : 'X';
+
+        System.out.print("Player 2, please enter your name: ");
+        String name2 = scanner.nextLine().trim();
+        System.out.print(name2 + ", your symbol is: " + symbol2);
+
+        String firstPlayer = (symbol1 == 'X') ? name1 : name2;
+        char firstSymbol = 'X';
+        String secondPlayer = (symbol1 == 'O') ? name1 : name2;
+        char secondSymbol = 'O';
+    }
 
     static void setupOnePlayer() {}
 
